92560838e0b6b59793a6b207d1a79b330192b85c
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-15 22:46:30 -0500

    Add files via upload

---
+++ JavaSeleniumInstagramLogin.java
@@ -0,0 +1,22 @@
+package sampleapp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Eray Şahin\\Desktop\\JAVA SELENIUM\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.instagram.com");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.findElement(By.name("username")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.name("username")).sendKeys("eeerrraaayyysssaaahhhiiinnn");
+        driver.findElement(By.name("password")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.name("password")).sendKeys("test1234");
+        driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div/div[3]/button/div")).click();
+
+    }
+}
